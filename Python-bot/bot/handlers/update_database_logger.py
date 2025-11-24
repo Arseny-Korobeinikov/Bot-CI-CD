13e42778624f470301e15ff15c85ec36ae3756e6
arseni.korobejnikov@yandex.ru <arseni.korobejnikov@yandex.ru>
2025-11-24 07:54:31 +0000

    merge with Pizza shop

---
+++ Python-bot/bot/handlers/update_database_logger.py
@@ -0,0 +1,16 @@
+import bot.database_client
+
+from bot.handlers.handler import Handler, HandlerStatus
+
+
+class UpdateDatabaseLogger(Handler):
+    """
+    Должен быть добавлен первым обработчиком
+    """
+
+    def can_handle(self, update: dict, state: str, data: dict) -> bool:
+        return True
+
+    def handle(self, update: dict, state: str, data: dict) -> HandlerStatus:
+        bot.database_client.persist_update(update)
+        return HandlerStatus.CONTINUE
